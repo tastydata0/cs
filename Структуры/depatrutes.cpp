0361e14d5fd917ebd263514dfda516d5b7666dc9
Alex <alex.zv.ev@gmail.com>
2023-04-30 10:45:50 +0300

    1

--- Структуры/depatrutes.cpp
+++ Структуры/depatrutes.cpp
@@ -11,31 +11,19 @@
 
 using namespace std;
 
-void clearScreen() {
-    cout << "\033[2J\033[1;1H";
-    // Если не работает на винде, пишите system("cls");
-}
+const string GRAY = "\033[90m";
+const string BLUE = "\033[94m";
+const string CYAN = "\033[96m";
+const string GREEN = "\033[92m";
+const string WARNING = "\033[93m";
+const string FAIL = "\033[91m";
+const string BOLD = "\033[1m";
+const string UNDERLINE = "\033[4m";
+const string ENDC = "\033[0m";
 
-void redner(vector<Departure>* departures) {
-    SimpleTime currentTime = getLocaltime();
-
-    int printed = 0;
-    for (int i = 0; i < departures->size() && printed <= 10; i++) {
-        if (!(departures->at(i)._time < currentTime)) {
-            departures->at(i).print();
-            printed++;
-        }
-    }
-}
-
-void updateInfinitely(vector<Departure>* departures, int delayMilliseconds) {
-    while (true) {
-        clearScreen();
-        redner(departures);
-        cout << "Updating..." << endl;
-        this_thread::sleep_for(chrono::milliseconds(delayMilliseconds));
-    }
-}
+const int STATUS_NONE = 0;
+const int STATUS_BOARDING = 1;
+const int STATUS_CANCEL = 2;
 
 struct SimpleTime {
     int hour;
@@ -77,13 +65,20 @@ struct Departure {
         destination = cities->at(rand() % cities->size());
     }
 
-    void print() {
+    void print(int status) {
         cout << "| " << setw(4) << left << id << " | ";
         cout << _time.toString() << " | ";
-        cout << setw(20) << left << destination << " | ";
-        cout << setw(4) << left << gate << " |" << endl;
-        cout << "|------|-------|----------------------|------|" << endl;
+        cout << WARNING << BOLD << setw(20) << left << destination << ENDC << " | ";
+        cout << setw(4) << left << gate << " | ";
 
+        if (status == STATUS_NONE)
+            cout << setw(8) << "" << " |" << endl;
+        else if (status == STATUS_CANCEL)
+            cout << setw(8) << "canceled" << " |" << endl;
+        else if (status == STATUS_BOARDING)
+            cout << setw(8) << "boarding" << " |" << endl;
+
+        cout << "|------|-------|----------------------|------|----------|" << endl;
     }
 
     bool operator<(const Departure& other) const {
@@ -91,10 +86,49 @@ struct Departure {
     }
 };
 
-void printHeader() {
-    cout << "|  ID  | TIME  | DESTINATION          | GATE |" << endl;
-    cout << "|------|-------|----------------------|------|" << endl;
+void clearScreen() {
+    cout << "\033[2J\033[1;1H";
+    // Если не работает на винде, пишите system("cls");
 }
+
+
+SimpleTime getLocaltime() {
+    time_t rawtime = time(0);
+    tm* timeinfo = localtime(&rawtime);
+    int hours = timeinfo->tm_hour;
+    int minutes = timeinfo->tm_min;
+
+    return {hours, minutes};
+}
+
+void printHeader() {
+    cout << "|  ID  | TIME  | DESTINATION          | GATE | STATUS   |" << endl;
+    cout << "|------|-------|----------------------|------|----------|" << endl;
+}
+
+void redner(vector<Departure>* departures) {
+    printHeader();
+
+    SimpleTime currentTime = getLocaltime();
+
+    int printed = 0;
+    for (int i = 0; i < departures->size() && printed <= 10; i++) {
+        if (!(departures->at(i)._time < currentTime)) {
+            departures->at(i).print(STATUS_BOARDING);
+            printed++;
+        }
+    }
+}
+
+void updateInfinitely(vector<Departure>* departures, int delayMilliseconds) {
+    while (true) {
+        clearScreen();
+        redner(departures);
+        cout << "Updating..." << endl;
+        this_thread::sleep_for(chrono::milliseconds(delayMilliseconds));
+    }
+}
+
 
 vector<string>* readCities() {
     vector<string>* cities = new vector<string>;
@@ -109,14 +143,6 @@ vector<string>* readCities() {
     return cities;
 }
 
-SimpleTime getLocaltime() {
-    time_t rawtime = time(0);
-    tm* timeinfo = localtime(&rawtime);
-    int hours = timeinfo->tm_hour;
-    int minutes = timeinfo->tm_min;
-
-    return {hours, minutes};
-}
 
 int main() {
     srand(time(0)); // Устанавливаем сид, равный системному времени
@@ -125,14 +151,16 @@ int main() {
 
     vector<Departure>* departures = new vector<Departure>(500);
 
-    printHeader();
+    
     for (Departure& d : *departures) 
         d = Departure(cities);
     
     sort(departures->begin(), departures->end());
 
+    updateInfinitely(departures, 1000);
+
     
-     cout << "\033[1;31mbold red text\033[0m\n";
+    cout << "\033[1;31mbold red text\033[0m\n";
     
     delete departures;
     delete cities;
